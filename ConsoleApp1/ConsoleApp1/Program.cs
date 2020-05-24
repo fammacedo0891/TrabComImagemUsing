7003b68adc6d91c9918e5ec7d677658254d6dc90
fammacedo0819 <fam_macedo@hotmail.com>
2020-05-24 14:15:37 -0300

    Deploy inicial

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = @"F:\Fernando\Curso\c#\Sistemas\TrabalhandoComArqUsing\File1.txt";
+
+           try
+            {
+                /*
+                 * usando o using para fechamento automatico do arq no final
+                 * sem a necessidade de utilizar o finally
+                 */
+                 
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (! sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+
+            }
+
+
+        }
+    }
+}
